c8ca8e1362522f46e2ef6953a08875caac8c505d
michivonah <info@michivonah>
2023-10-07 21:40:02 +0200

    add refresh time to reduce api requests

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ load_dotenv()
 # Global defintions
 subscribedAttractions = ["383533", "383530", "323530", "323030", "353030", "393030"]
 currentTimes = {}
+refreshTime = 30
 
 # Send messagess via Discord Webhook
 def sendMessage(message):
@@ -35,12 +36,15 @@ def checkTimes(subscribedAttractions):
     for attraction in attractions:
         if attraction["code"] in subscribedAttractions:
             if attraction["status"] == "opened":
+                refreshTime = 30
                 if not attraction["code"] in currentTimes: currentTimes[attraction["code"]] = attraction["waitingtime"];
                 if currentTimes[attraction["code"]] > attraction["waitingtime"]:
                     sendMessage(f"Waiting time of {attraction['name']} sank to {attraction['waitingtime']} Minutes!")
                 elif currentTimes[attraction["code"]] < attraction["waitingtime"]:
                     sendMessage(f"Waiting time for {attraction['name']} increased to {attraction['waitingtime']} Minutes!")
                 currentTimes[attraction["code"]] = attraction["waitingtime"]
+            else:
+                refreshTime = 180
 
 # Main Loop
 # Checks every 30 seconds for changes in the waiting times of the subscribed attractions
@@ -51,5 +55,5 @@ if __name__ == '__main__':
     while True:
         checkTimes(subscribedAttractions)
         print(f"Checked for updates at {time.strftime('%H:%M:%S', time.localtime())}")
-        time.sleep(30)
+        time.sleep(refreshTime)
 
